4ce0e1263604239aef610c9db69a83465fa4dc81
Ekaterina Zhulina <y.zhulina@SMRTNBSCDBU>
2024-07-19 09:42:52 +0300

    gg

--- DataBased/Services/ClientRequestService.cs
+++ DataBased/Services/ClientRequestService.cs
@@ -17,15 +17,12 @@ namespace TranscriberApi.DataBased.Services
         {
             using var connection = _context.CreateConnection();
             var query = @"
-                SELECT c.FullName AS ClientFullName, 
-                       cl.FullName AS CallerFullName, 
-                       LoanAmount
-                FROM Clients c
-                INNER JOIN Calls cl ON c.PhoneNumber = cl.PhoneNumber
-                WHERE c.PhoneNumber = @PhoneNumber"; //add param start time
+                SELECT * FROM Cube_MoneyRent_Orchard_Cabinets_Collection_CollectionCalling cc
+                INNER JOIN Cube_MoneyRent_Orchard_RequestForm_ContactDataRecord cdr ON cdr.Id = cc.RentRequestId
+                WHERE cdr.MobilePhone = @phoneNumber AND 
+	            cc.DateTime BETWEEN @startDate AND @endDate"; 
 
-            
-            var parameters = new { PhoneNumber = phoneNumber};
+            var parameters = new { PhoneNumber = phoneNumber };
             var result = await connection.QuerySingleOrDefaultAsync<ClientRequestModel>(query, parameters);
 
             return result!;
